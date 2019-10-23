21da9fe082124abb943c9acb457bde140090ef33
fahim zuhair <fahim.zuhair@opencyon.com>
2019-10-23 17:38:31 +0530

    removed unwanted page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'track', loadChildren: './track/track.module#TrackPageModule' },
+  { path: '', redirectTo: 'track', pathMatch: 'full' },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'track', loadChildren: './track/track.module#TrackPageModule' }
 ];
 
 @NgModule({
@@ -13,4 +13,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
